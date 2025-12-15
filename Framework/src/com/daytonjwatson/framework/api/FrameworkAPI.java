e3b195fd9ad37ec54d60362307777ffc0ae7ce30
DaytonJWatson <45436514+DaytonJWatson@users.noreply.github.com>
2025-12-15 12:41:54 -0700

    Expand FrameworkAPI methods

--- Framework/src/com/daytonjwatson/framework/api/FrameworkAPI.java
+++ Framework/src/com/daytonjwatson/framework/api/FrameworkAPI.java
@@ -6,6 +6,7 @@ import com.daytonjwatson.framework.utils.MessageHandler;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.util.Map;
 import java.util.Set;
 
 public class FrameworkAPI {
@@ -31,6 +32,10 @@ public class FrameworkAPI {
         return storageManager.getHomes(player);
     }
 
+    public void deleteHome(Player player, String name) {
+        storageManager.deleteHome(player, name);
+    }
+
     public void setWarp(String name, Location location) {
         storageManager.setWarp(name, location);
     }
@@ -41,6 +46,70 @@ public class FrameworkAPI {
 
     public Set<String> getWarps() {
         return storageManager.getWarps();
+    }
+
+    public void deleteWarp(String name) {
+        storageManager.deleteWarp(name);
+    }
+
+    public void addWarning(String playerName, String reason, String issuedBy) {
+        storageManager.addWarning(playerName, reason, issuedBy);
+    }
+
+    public Map<Integer, Map<String, String>> getWarnings(String playerName) {
+        return storageManager.getWarnings(playerName);
+    }
+
+    public void clearWarnings(String playerName) {
+        storageManager.clearWarnings(playerName);
+    }
+
+    public void addBan(String playerName, String reason, String issuedBy, long durationMillis) {
+        storageManager.addBan(playerName, reason, issuedBy, durationMillis);
+    }
+
+    public boolean isBanned(String playerName) {
+        return storageManager.isBanned(playerName);
+    }
+
+    public void removeBan(String playerName) {
+        storageManager.removeBan(playerName);
+    }
+
+    public long getBanExpiry(String playerName) {
+        return storageManager.getBanExpiry(playerName);
+    }
+
+    public Set<String> getBannedPlayers() {
+        return storageManager.getBannedPlayers();
+    }
+
+    public String getBanReason(String playerName) {
+        return storageManager.getBanReason(playerName);
+    }
+
+    public void addMute(String playerName, String reason, String issuedBy, long durationMillis) {
+        storageManager.addMute(playerName, reason, issuedBy, durationMillis);
+    }
+
+    public boolean isMuted(String playerName) {
+        return storageManager.isMuted(playerName);
+    }
+
+    public void removeMute(String playerName) {
+        storageManager.removeMute(playerName);
+    }
+
+    public long getMuteExpiry(String playerName) {
+        return storageManager.getMuteExpiry(playerName);
+    }
+
+    public String getMuteReason(String playerName) {
+        return storageManager.getMuteReason(playerName);
+    }
+
+    public Set<String> getMutedPlayers() {
+        return storageManager.getMutedPlayers();
     }
 
     public MessageHandler getMessageHandler() {
